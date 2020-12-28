187be7346d3cc4a501966c5772afe6ace29dbcf6
vuvoivang <vuprotk21@gmail.com>
2020-12-28 16:52:06 +0700

    first commit

---
+++ owl-carousel.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('.owl-carousel-1').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+});
+$(document).ready(function () {
+    $('.owl-carousel-2').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+});
